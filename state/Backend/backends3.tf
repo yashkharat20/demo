cf86b678530f40166b8f193445e3bef6708f9928
yashkharat20 <yash.kharat@ltm.com>
2026-06-27 11:02:25 +0530

    Update backends3.tf

--- state/Backend/backends3.tf
+++ state/Backend/backends3.tf
@@ -1,13 +1,13 @@
-resource "aws_s3_bucket" "terrayashs3" {
-    bucket = "terrayashs3"
+resource "aws_s3_bucket" "terrayaaashs3" {
+    bucket = "terrayaaashs3"
 
     tags = {
-        Name = "terrayashs3"
+        Name = "terrayaaashs3"
     }
 }
 
 resource "aws_s3_bucket_versioning" "terra-versioning"{
-    bucket = aws_s3_bucket.terrayashs3.id
+    bucket = aws_s3_bucket.terrayaaashs3.id
 
     versioning_configuration {
         status = "Enabled"
