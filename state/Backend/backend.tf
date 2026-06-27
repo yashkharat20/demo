27dece9bb031d20c918870f21ae761293547b269
yashkharat20 <yash.kharat@ltm.com>
2026-06-27 11:03:03 +0530

    Update backend.tf

--- state/Backend/backend.tf
+++ state/Backend/backend.tf
@@ -1,6 +1,6 @@
 terraform {
     backend "s3" {
-        bucket = "terrayashs3"
+        bucket = "terrayaaashs3"
         key = "dev/vpc/terrafomr.tfstate"
         region = "ap-south-1"
         dynamodb_table = "terraform-lock-table"
